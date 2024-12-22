e6d84541bab24cf7c826fcc82c1326fca4f31a0c
Jake Soenneker <jake@soenneker.com>
2024-12-22 07:47:36 -0600

    Tests

---
+++ test/Soenneker.Constants.Data.Tests/DataConstantsTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Constants.Data.Tests;
+
+public class DataConstantsTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
